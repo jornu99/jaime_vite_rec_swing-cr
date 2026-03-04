56e2d8b6cc7d1d5da23bbd0dd57292f409a04123
Jaime <jaime.ornu@gmail.com>
2026-03-04 10:51:46 +0100

    feat: main generando todo al llamar a AppController y los estilos de todas las cosas (estilos realizados con Perplexity)

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
-import './style.css'
+import { AppController } from './controllers/appController.js';
+import './css/base.css';
+import './css/layout.css';
+import './css/form.css';
+import './css/calendar.css';
 
-document.querySelector('#app').innerHTML = `
-  
-`
+new AppController();
